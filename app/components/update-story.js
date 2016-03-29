831e57b6db81a1b2fee44b4804ca52555672e597
Afton Downey and Bri Popson <aaike001@gmail.com>
2016-03-29 16:31:31 -0700

    created update-story with method connecting rental-tile

--- app/components/update-story.js
+++ app/components/update-story.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateStoryForm: false,
+  actions: {
+    updateStoryForm() {
+      this.set('updateStoryForm', true);
+    },
+    update(story) {
+      var params = {
+        title: this.get('title'),
+        content: this.get('content'),
+        img: this.get('img'),
+        category: this.get('category'),
+        author: this.get('author'),
+      };
+      this.set('updateStoryForm', false);
+      this.sendAction('update', story, params);
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,14 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('story');
   }
+  actions:{
+  update(rental, params) {
+  this.sendAction('update', rental, params);
+  },
+  delete(rental) {
+    if (confirm('Are you sure you want to delete this rental?')) {
+      this.sendAction('destroyRental', rental);
+    }
+  }
+}
 });
